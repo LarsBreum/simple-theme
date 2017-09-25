0fa50852318598c8dc845f1794949514c9fe0424
Lars Hansen <lars.breum.hansen@live.dk>
2017-09-25 22:44:39 +0200

    basic gulp workflow set up

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('workflow', function () {
+  gulp.src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('./'))
+
+  .pipe(gulp.dest('./dist/css/'))
+});
+
+gulp.task('default', function () {
+  gulp.watch('./src/scss/**/*.scss', ['workflow']);
+});
