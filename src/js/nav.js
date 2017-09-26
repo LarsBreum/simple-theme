0900ee380ead3bbcf955b64fe954a4d67374ab85
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-09-26 22:08:13 +0200

    front page good above fold

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,8 @@
 const $nav = $('.nav');
 const $burgerIcon = $('.burger-icon');
-
+const bars = $('.bar1, .bar2, .bar3');
+console.log(bars);
 $burgerIcon.on('click', () => {
+	bars.toggleClass('change');
 	$nav.toggle();
 });
